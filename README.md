db622c9413167c88a95e19eb415b2b0eea25f297
youngUnPark <mduddns@naver.com>
2021-07-21 20:06:20 +0900

    chore: security config change, add swagger

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # z1zon-todo-server
 지존 서버!
+
+## Swagger
+
+/swagger-ui.html
---
+++ src/main/java/com/nnlk/z1zontodoserver/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.nnlk.z1zontodoserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 필터 등록해서 controller로 들어오는 요청 필터링
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
