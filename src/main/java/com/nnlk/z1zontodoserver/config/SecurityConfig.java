87d7f84e68ae65e1943371093e4a1f015cf810d1
youngUnPark <mduddns@naver.com>
2021-07-26 23:07:12 +0900

    feat: add h2 security config

--- src/main/java/com/nnlk/z1zontodoserver/config/SecurityConfig.java
+++ src/main/java/com/nnlk/z1zontodoserver/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity // 필터 등록해서 controller로 들어오는 요청 필터링
@@ -14,6 +18,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .csrf()
+                .ignoringAntMatchers("/h2/**") //h2 csrf 처리
+                .and()
+                .headers()
+                .addHeaderWriter(
+                        new XFrameOptionsHeaderWriter(
+                                new WhiteListedAllowFromStrategy(Arrays.asList("localhost"))    // 여기!
+                        )
+                )
+                .frameOptions().sameOrigin();
+
     }
 }
