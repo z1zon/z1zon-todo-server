67b6eca90a5acb82d381dbfc7e53f74112b3e308
Kimakjun <49400477+Kimakjun@users.noreply.github.com>
2021-09-29 21:37:33 +0900

    task crud api 작성 (#32)
    #12

---
+++ src/main/java/com/nnlk/z1zontodoserver/dto/task/TaskUpsertRequestDto.java
@@ -0,0 +1,51 @@
+package com.nnlk.z1zontodoserver.dto.task;
+
+import com.nnlk.z1zontodoserver.domain.Category;
+import com.nnlk.z1zontodoserver.domain.Task;
+import com.nnlk.z1zontodoserver.domain.TaskStatus;
+import com.nnlk.z1zontodoserver.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class TaskUpsertRequestDto {
+
+    @NotNull(message = "name은 필수 값 입니다.")
+    private String content;
+
+    @NotNull(message = "startAt 값은 필수 값 입니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startAt;
+
+    @NotNull(message = "endAt 값은 필수 값 입니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endAt;
+
+    private String color;
+
+    private Integer importance;
+
+    private Long categoryId;
+
+    private TaskStatus taskStatus;
+
+    public Task toEntity(User user, Category category) {
+
+        return Task.builder()
+                .content(content)
+                .importance(importance)
+                .color(color)
+                .taskStatus(TaskStatus.TODO)
+                .category(category)
+                .user(user)
+                .startAt(startAt)
+                .endAt(endAt)
+                .build();
+    }
+
+}
