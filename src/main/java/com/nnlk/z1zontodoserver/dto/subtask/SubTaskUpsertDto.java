3931d0ff135c31936a5678a267a01858bad4aae0
hakjun-kim <dkd7142@naver.com>
2021-10-09 18:41:37 +0900

    subtask dto 작성

---
+++ src/main/java/com/nnlk/z1zontodoserver/dto/subtask/SubTaskUpsertDto.java
@@ -0,0 +1,29 @@
+package com.nnlk.z1zontodoserver.dto.subtask;
+
+import com.nnlk.z1zontodoserver.domain.SubTask;
+import com.nnlk.z1zontodoserver.domain.Task;
+import com.nnlk.z1zontodoserver.domain.TaskStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@AllArgsConstructor
+public class SubTaskUpsertDto {
+    @NotNull(message = "content은 필수 값 입니다.")
+    private String content;
+
+    private TaskStatus taskStatus;
+
+    private Long taskId;
+
+    public SubTask toEntity(Task task) {
+        return SubTask.builder()
+                .content(content)
+                .taskStatus(TaskStatus.TODO)
+                .task(task)
+                .build();
+    }
+
+}
